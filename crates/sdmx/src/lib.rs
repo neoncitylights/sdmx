2ffa0152a3ecc4ead336c3405472d19b1c81217a
Samantha Nguyen <contact@samanthanguyen.me>
2024-12-13 23:55:09 -0600

    feat: 1st pass of initial source code

--- crates/sdmx/src/lib.rs
+++ crates/sdmx/src/lib.rs
@@ -1,39 +1,424 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
-//! This is a top-level file comment (`//!`). In `lib.rs` specifically,
-//! it shows up as the root-level crate documentation.
-//!
-//! For documentation about the different types of Doc comments in Rust,
-//! see <https://cheats.rs/#documentation>
+//! A Rust implementation of SDMX-JSON (Statistical Data and Metadata eXchange)
+//! using Serde.
 
-/// This is an outer-line doc comment (triple slash, `///`).
-/// This documentation gets attached to the symbol in Rustdoc,
-/// and shows up in generated documentation.
-///
-/// Say hi to someone!
-///
-/// # Examples
-/// This code block gets ran by Rustdoc and is considered a "doctest".
-/// ```
-/// use sdmx::greet;
-///
-/// assert_eq!(greet("Bob"), String::from("Hello Bob!"));
-/// ```
-pub fn greet(name: &str) -> String {
-	format!("Hello {}!", name)
+use serde::Deserialize;
+use serde::Serialize;
+use serde_json::Value;
+use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Default)]
+pub struct MetadataMessage {
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub meta: Option<Meta>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub data: Option<Data>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub error: Option<Error>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	#[serde(flatten)]
+	pub other: Option<HashMap<String, Value>>,
 }
 
-#[cfg(test)]
-mod tests {
-	use crate::greet;
-
-	#[test]
-	fn greet_bob() {
-		assert_eq!(greet("Bob"), String::from("Hello Bob!"));
-	}
-
-	#[test]
-	fn greet_the_world() {
-		assert_eq!(greet("World"), String::from("Hello World!"));
-	}
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+pub struct Meta {
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub schema: Option<String>,
+	pub id: String,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub test: Option<bool>,
+	pub prepared: String,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	#[serde(rename = "contentLanguages")]
+	pub content_languages: Option<Vec<String>>,
+	pub name: String,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub names: Option<LocalizedText>,
+	pub sender: Sender,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub receiver: Option<Vec<Receiver>>,
+	pub links: Vec<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	#[serde(flatten)]
+	pub other: Option<HashMap<String, Value>>,
 }
+
+pub type Sender = Exchanger;
+pub type Receiver = Exchanger;
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Default)]
+pub struct Exchanger {
+	pub id: String,
+	pub name: String,
+	pub names: LocalizedText,
+	pub contacts: Vec<Contact>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	#[serde(flatten)]
+	pub other: Option<HashMap<String, Value>>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Default)]
+pub struct Contact {
+	pub id: String,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub name: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub names: Option<LocalizedText>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub department: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub departments: Option<LocalizedText>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub role: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub roles: Option<LocalizedText>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub telephones: Option<Vec<String>>,
+	pub faxes: Option<Vec<String>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub uris: Option<Vec<String>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub emails: Option<Vec<String>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub x400s: Option<Vec<String>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	#[serde(flatten)]
+	pub other: Option<HashMap<String, Value>>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Default)]
+pub struct Data {
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub structures: Option<Structure>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	#[serde(rename = "dataSets")]
+	pub data_sets: Option<DataSet>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	#[serde(flatten)]
+	pub other: Option<HashMap<String, Value>>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Default)]
+pub struct Structure {
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub links: Option<Vec<String>>,
+	pub dimensions: Dimensions,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub measures: Option<Measures>,
+	pub attributes: Attributes,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub annotations: Option<Vec<Annotation>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub datasets: Option<DataSet>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	#[serde(flatten)]
+	pub other: Option<HashMap<String, Value>>,
+}
+
+pub type Dimensions = DimsMeasuresAttributes;
+pub type Measures = DimsMeasuresAttributes;
+pub type Attributes = DimsMeasuresAttributes;
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Default)]
+pub struct DimsMeasuresAttributes {
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub data_set: Option<Vec<Component>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub dimension_group: Option<Vec<Component>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub series: Option<Vec<Component>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub observation: Option<Vec<Component>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	#[serde(flatten)]
+	pub other: Option<HashMap<String, Value>>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Default)]
+pub struct Component {
+	pub id: String,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub name: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub names: Option<LocalizedText>,
+	pub description: String,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub descriptions: Option<LocalizedText>,
+	pub key_position: usize,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub roles: Option<Vec<String>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	#[serde(rename = "isMandatory")]
+	pub is_mandatory: Option<bool>,
+	pub relationship: AttributeRelationship,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub format: Option<Format>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub default_value: Option<NumberOrString>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub links: Option<Vec<String>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub annotations: Option<Vec<String>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub values: Option<Vec<ComponentValue>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	#[serde(flatten)]
+	pub other: Option<HashMap<String, Value>>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Default)]
+pub struct AttributeRelationship {
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub dataflow: Option<Value>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub dimensions: Option<Vec<String>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub observation: Option<Value>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	#[serde(rename = "primaryMeasure")]
+	pub primary_measure: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub measures: Option<Vec<String>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	#[serde(flatten)]
+	pub other: Option<HashMap<String, Value>>,
+}
+
+#[serde(rename_all = "camelCase")]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Default)]
+pub struct Format {
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub min_occurs: Option<usize>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub max_occurs: Option<usize>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub data_type: Option<DataType>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub is_sequence: Option<bool>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub interval: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub start_time: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub end_time: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub min_length: Option<usize>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub max_length: Option<usize>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub min_value: Option<isize>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub max_value: Option<isize>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub decimals: Option<usize>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub is_multilingual: Option<bool>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub sentinel_values: Option<Vec<NumberOrString>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	#[serde(flatten)]
+	pub other: Option<HashMap<String, Value>>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+pub enum DataType {
+	String,
+	Alpha,
+	AlphaNumeric,
+	Numeric,
+	BigInteger,
+	Integer,
+	Long,
+	Short,
+	Decimal,
+	Float,
+	Double,
+	Boolean,
+	#[serde(rename = "URI")]
+	Uri,
+	Count,
+	InclusiveValueRange,
+	ExclusiveValueRange,
+	GregorianTimePeriod,
+	GregorianYear,
+	GregorianYearMonth,
+	GregorianDay,
+	ReportingTimePeriod,
+	ReportingYear,
+	ReportingSemester,
+	ReportingTrimester,
+	ReportingQuarter,
+	ReportingMonth,
+	ReportingWeek,
+	ReportingDay,
+	DateTime,
+	TimeRange,
+	Month,
+	MonthDay,
+	Day,
+	Time,
+	Duration,
+	GeospatialInformation,
+	#[serde(rename = "XHTML")]
+	Xhtml,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Default)]
+pub struct SentinelValue {
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub value: Option<NumberOrString>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub name: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub names: Option<LocalizedText>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub description: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub descriptions: Option<LocalizedText>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	#[serde(flatten)]
+	pub other: Option<HashMap<String, Value>>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+pub enum NumberOrString {
+	Number(isize),
+	String(String),
+}
+
+#[serde(rename_all = "camelCase")]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Default)]
+pub struct ComponentValue {
+	pub id: String,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub name: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub names: Option<LocalizedText>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub values: Option<Vec<SdmxValue>>,
+	pub description: String,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub descriptions: Option<LocalizedText>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub start: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub end: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub parent: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub order: Option<isize>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub links: Option<Vec<String>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub annotations: Option<Vec<Annotation>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	#[serde(flatten)]
+	pub other: Option<HashMap<String, Value>>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+pub enum SdmxValue {
+	String(String),
+	Number(isize),
+	Boolean(bool),
+	LocalizedText(LocalizedText),
+	Array(Box<Vec<SdmxValue>>),
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Default)]
+pub struct Annotation {
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub id: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub title: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	#[serde(rename = "type")]
+	pub type_: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub value: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub text: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub texts: Option<LocalizedText>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub links: Option<Vec<String>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	#[serde(flatten)]
+	pub other: Option<HashMap<String, Value>>,
+}
+
+#[serde(rename_all = "camelCase")]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Default)]
+pub struct DataSet {
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub structure: Option<isize>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub action: Option<Action>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub reporting_begin: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub reporting_end: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub valid_from: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub valid_to: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub publication_year: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub publication_period: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub links: Option<Vec<String>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub annotations: Option<Vec<Annotation>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub attributes: Option<Vec<Attributes>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub dimension_group_attributes: Option<SdmxObject>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub series: Option<Vec<Series>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub observations: Option<SdmxObject>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	#[serde(flatten)]
+	pub other: Option<HashMap<String, Value>>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Action {
+	Append,
+	Replace,
+	Delete,
+	Information,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Default)]
+pub struct SdmxObject(pub HashMap<String, SdmxValue>);
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Default)]
+pub struct Series {
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub annotations: Option<Vec<Annotation>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub attributes: Option<Vec<SdmxValue>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub observations: Option<SdmxObject>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	#[serde(flatten)]
+	pub other: Option<HashMap<String, Value>>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Default)]
+pub struct Error {
+	pub code: usize,
+	pub title: String,
+	pub titles: LocalizedText,
+	pub detail: String,
+	pub details: LocalizedText,
+	pub links: Vec<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	#[serde(flatten)]
+	pub other: Option<HashMap<String, Value>>,
+}
+
+pub type LocalizedText = HashMap<String, String>;
