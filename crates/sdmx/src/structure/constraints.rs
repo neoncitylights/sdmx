0a2c215c3f533a4603915cecfc3296cbf4a44fee
Samantha Nguyen <contact@samanthanguyen.me>
2024-12-14 16:57:35 -0600

    don't serialize empty things

--- crates/sdmx/src/structure/constraints.rs
+++ crates/sdmx/src/structure/constraints.rs
@@ -8,9 +8,13 @@ pub struct DataConstraint {
 	#[serde(flatten)]
 	pub artefact: CommonArtefactType,
 	pub role: String,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub constraint_attachment: Option<ConstraintAttachment>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub cube_regions: Option<Vec<CubeRegion>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub data_key_sets: Option<Vec<DataKeySet>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub release_calendar: Option<ReleaseCalendar>,
 }
 
@@ -20,31 +24,47 @@ pub struct MetadataConstraint {
 	#[serde(flatten)]
 	pub artefact: CommonArtefactType,
 	pub role: String,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub constraint_attachment: Option<MetadataConstraintAttachment>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub metadata_target_regions: Option<Vec<MetadataTargetRegion>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub release_calendar: Option<ReleaseCalendar>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct ConstraintAttachment {
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub data_provider: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub data_structures: Option<Vec<String>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub dataflows: Option<Vec<String>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub provision_agreements: Option<Vec<String>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub simple_data_sources: Option<Vec<String>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub queryable_data_sources: Option<QueryableDataSource>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct MetadataConstraintAttachment {
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub metadata_provider: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub metadata_sets: Option<Vec<String>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub metadata_structures: Option<Vec<String>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub metadataflows: Option<Vec<String>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub metadata_provision_agreements: Option<Vec<String>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub simple_data_sources: Option<Vec<String>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub queryable_data_sources: Option<QueryableDataSource>,
 }
 
@@ -53,18 +73,26 @@ pub struct MetadataConstraintAttachment {
 pub struct QueryableDataSource {
 	pub is_rest_datasource: bool,
 	pub is_web_service_datasource: bool,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub data_url: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub wadl_url: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub wsdl_url: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct CubeRegion {
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub annotations: Option<Vec<Annotation>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub links: Option<Links>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub include: Option<bool>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub components: Option<Vec<ComponentValueSet>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub key_values: Option<Vec<CubeRegionKey>>,
 }
 
@@ -72,9 +100,13 @@ pub struct CubeRegion {
 #[serde(rename_all = "camelCase")]
 pub struct ComponentValueSet {
 	pub id: String,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub include: Option<bool>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub remove_prefix: Option<bool>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub time_range: Option<TimeRangeValue>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub values: Option<Vec<StringOrScv>>,
 }
 
@@ -88,16 +120,22 @@ pub enum StringOrScv {
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct TimeRangeValue {
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub after_period: Option<TimePeriodRange>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub before_period: Option<TimePeriodRange>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub end_period: Option<TimePeriodRange>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub start_period: Option<TimePeriodRange>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct TimePeriodRange {
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub period: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub is_inclusive: Option<bool>,
 }
 
@@ -105,9 +143,12 @@ pub struct TimePeriodRange {
 #[serde(rename_all = "camelCase")]
 pub struct SimpleComponentValue {
 	pub value: String,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub lang: Option<String>,
 	pub cascade_values: CascadeValues,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub valid_from: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub valid_to: Option<String>,
 }
 
@@ -122,11 +163,17 @@ pub enum CascadeValues {
 #[serde(rename_all = "camelCase")]
 pub struct CubeRegionKey {
 	pub id: String,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub include: Option<bool>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub remove_prefix: Option<bool>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub valid_from: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub valid_to: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub time_range: Option<TimeRangeValue>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub values: Option<Vec<StringOrScv>>,
 }
 
@@ -140,10 +187,14 @@ pub struct DataKeySet {
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct DataKey {
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub annotations: Option<Vec<Annotation>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub links: Option<Links>,
 	pub include: bool,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub valid_from: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub valid_to: Option<String>,
 	pub key_values: Vec<DataKeyValue>,
 	pub components: Vec<DataComponentValueSet>,
@@ -153,7 +204,9 @@ pub struct DataKey {
 #[serde(rename_all = "camelCase")]
 pub struct DataKeyValue {
 	pub id: String,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub include: Option<bool>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub remove_prefix: Option<bool>,
 	pub value: String,
 }
@@ -162,8 +215,11 @@ pub struct DataKeyValue {
 #[serde(rename_all = "camelCase")]
 pub struct DataComponentValueSet {
 	pub id: String,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub include: Option<bool>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub remove_prefix: Option<bool>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub time_range: Option<TimeRangeValue>,
 	pub values: Vec<StringOrDcv>,
 }
@@ -178,7 +234,9 @@ pub enum StringOrDcv {
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct DataComponentValue {
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub cascade_values: Option<CascadeValues>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub lang: Option<String>,
 	pub value: String,
 }
@@ -186,11 +244,17 @@ pub struct DataComponentValue {
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct MetadataTargetRegion {
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub annotations: Option<Vec<Annotation>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub links: Option<Links>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub include: Option<bool>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub components: Option<Vec<MetadataAttributeValueSet>>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub valid_from: Option<String>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub valid_to: Option<String>,
 }
 
@@ -198,9 +262,13 @@ pub struct MetadataTargetRegion {
 #[serde(rename_all = "camelCase")]
 pub struct MetadataAttributeValueSet {
 	pub id: String,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub include: Option<bool>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub remove_prefix: Option<bool>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub time_range: Option<TimeRangeValue>,
+	#[serde(skip_serializing_if = "Option::is_none")]
 	pub values: Option<Vec<StringOrScv>>,
 }
 
