a596bb0155e3efa1d992c28ed5bf3f1941f67fc2
Samantha <contact@samanthanguyen.me>
2024-12-14 22:34:37 -0600

    tests: add tests for metadata format (#14)

--- crates/sdmx-de-tests/src/lib.rs
+++ crates/sdmx-de-tests/src/lib.rs
@@ -1,4 +1,3 @@
-use sdmx::data::DataMessage;
 use std::error::Error;
 use std::fmt::Display;
 use std::fs::read_to_string;
@@ -20,9 +19,12 @@ impl Display for ReadJsonError {
 	}
 }
 
-pub fn read_json(path: &str) -> Result<DataMessage, ReadJsonError> {
+pub fn read_json<T>(path: &str) -> Result<T, ReadJsonError>
+where
+	T: FromStr<Err = serde_json::Error>,
+{
 	match read_to_string(path) {
-		Ok(s) => DataMessage::from_str(&s).map_err(ReadJsonError::Serde),
+		Ok(s) => T::from_str(&s).map_err(ReadJsonError::Serde),
 		Err(e) => Err(ReadJsonError::IO(e)),
 	}
 }
@@ -37,18 +39,41 @@ macro_rules! fixture {
 #[cfg(test)]
 mod tests {
 	use super::*;
+	use sdmx::data::DataMessage;
+	use sdmx::metadata::MetadataMessage;
 
 	#[test]
 	#[cfg_attr(miri, ignore)]
-	fn test_sample01() {
-		let file = read_json(fixture!("sample01.json"));
+	fn test_data_sample01() {
+		let file = read_json::<DataMessage>(fixture!("data_sample01.json"));
 		assert!(file.is_ok());
 	}
 
 	#[test]
 	#[cfg_attr(miri, ignore)]
-	fn test_sample02() {
-		let file = read_json(fixture!("sample02.json"));
+	fn test_data_sample02() {
+		let file = read_json::<DataMessage>(fixture!("data_sample02.json"));
+		println!("{:?}", file);
+	}
+
+	#[test]
+	#[cfg_attr(miri, ignore)]
+	fn test_metadata_sample01() {
+		let file = read_json::<MetadataMessage>(fixture!("metadata_sample01.json"));
+		println!("{:?}", file);
+	}
+
+	#[test]
+	#[cfg_attr(miri, ignore)]
+	fn test_metadata_sample02() {
+		let file = read_json::<MetadataMessage>(fixture!("metadata_sample02.json"));
+		println!("{:?}", file);
+	}
+
+	#[test]
+	#[cfg_attr(miri, ignore)]
+	fn test_metadata_sample03() {
+		let file = read_json::<MetadataMessage>(fixture!("metadata_sample03.json"));
 		println!("{:?}", file);
 	}
 }
